2f732440b52616f8a99bb8141f4f06bfe51ab44a
nahidhk <nahidappltd@gmail.com>
2024-09-03 17:46:38 +0600

    new update

---
+++ appsno1/lib/1day.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+          centerTitle: true,
+          leading: Icon(Icons.message),
+
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: (){
+              },
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
